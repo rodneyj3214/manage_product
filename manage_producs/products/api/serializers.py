1cc5d1e7bd1481c93ca2da6ab3ef00539d8c88f6
Rodney Jácome <rodneyj3214@gmail.com>
2023-01-27 00:03:58 -0500

    RJ - Notify Admins Users in change products

--- manage_producs/products/api/serializers.py
+++ manage_producs/products/api/serializers.py
@@ -1,6 +1,8 @@
+from django.core.mail import EmailMultiAlternatives
 from rest_framework import serializers
 
 from manage_producs.products.models import Product
+from manage_producs.users.models import User
 
 
 class ProductModelSerializer(serializers.ModelSerializer):
@@ -11,3 +13,20 @@ class ProductModelSerializer(serializers.ModelSerializer):
             "brand",
             "price",
         )
+
+    def update(self, instance, validated_data):
+        result = super().update(instance, validated_data)
+        self.send_notification_email(instance)
+        return result
+
+    def send_notification_email(self, instance):
+        email_list = []
+        for user in User.objects.all():
+            email_list.append(user.email)
+        subject = f"Alert! this product was change {instance.name}"
+        from_email = "from@example.com"
+        text_content = "This message is for notify that this product was change."
+        html_content = "This message is for notify that this product was change."
+        msg = EmailMultiAlternatives(subject, text_content, from_email, email_list)
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
--- manage_producs/products/models.py
+++ manage_producs/products/models.py
@@ -6,7 +6,14 @@ class Product(models.Model):
     brand = models.CharField("Brand", max_length=255)
     price = models.FloatField("Price", default=0)
 
-    # picture = models.ImageField(upload_to="products/pictures", blank=True, null=True)
-
     def __str__(self):
         return self.name
+
+
+class HistorySearchProduct(models.Model):
+    ip_address = models.CharField("IP Address", max_length=140)
+    creation_date = models.DateTimeField("Brand", auto_now_add=True)
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"#{self.ip_address}: {self.product.name}"
